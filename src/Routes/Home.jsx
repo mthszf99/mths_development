26c0852003e1768f70f7adb50a46e7c656b39815
mthszf99 <matheusf99@outlook.com>
2024-10-24 14:10:16 -0300

    h2 and p (home)

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from '../Components/Header/Header'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='Page'>
+      <h2>Transformando Ideias <br/> Em Experiências <br/> Digitais Inovadoras</h2>
+      <p>Combinando design intuitivo e tecnologia de ponta,<br/> criamos soluções digitais que entregam resultados.<br/> Cada projeto é uma oportunidade de transformar a<br/> visão do cliente em uma experiência única, funcional e<br/> impactante, elevando sua presença digital ao próximo<br/> nível.</p>
     </div>
   )
 }
