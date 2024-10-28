17a9ef54a0837437e9cce3bc7124acd45b39d68f
mthszf99 <matheusf99@outlook.com>
2024-10-28 15:05:16 -0300

    initial css

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './HomeStyles.css';
+import fonts from 'google-fonts';
+
+fonts.add({
+  'DotGothic16' : ['400', 'sans-serif']
+})
 
 function Home() {
   return (
-    <div className='Page'>
+    <div>
       <h2>Transformando Ideias <br/> Em Experiências <br/> Digitais Inovadoras</h2>
       <p>Combinando design intuitivo e tecnologia de ponta,<br/> criamos soluções digitais que entregam resultados.<br/> Cada projeto é uma oportunidade de transformar a<br/> visão do cliente em uma experiência única, funcional e<br/> impactante, elevando sua presença digital ao próximo<br/> nível.</p>
     </div>
