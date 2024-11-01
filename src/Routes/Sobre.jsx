f5d35094d22c2963bba6c1ce7a4f159ff692fcf1
Matheus <matheusf99@outlook.com>
2024-11-01 14:30:24 -0300

    implement swiper

--- src/Routes/Sobre.jsx
+++ src/Routes/Sobre.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {IMG02} from '../images/IMG01.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css';
 
 function Sobre() {
   return (
     <div>
       <h1>Sobre</h1>
+      <img src={IMG02 }/>
+      <Swiper>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
     </div>
   )
 }
