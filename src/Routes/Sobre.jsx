b23575634ea4e6f9874c7f081dbcb4b11f88fa3d
Matheus <matheusf99@outlook.com>
2024-11-01 15:54:28 -0300

    update

--- src/Routes/Sobre.jsx
+++ src/Routes/Sobre.jsx
@@ -12,6 +12,7 @@ function Sobre() {
       Nosso compromisso é com a excelência em cada<br/> detalhe. Acreditamos que cada cliente tem uma<br/> visão única, e é nossa missão transformar essa<br/> visão em realidade por meio de uma abordagem<br/> colaborativa e orientada por resultados. Desde a<br/> concepção até a entrega final, garantimos um<br/> serviço de alta qualidade, sempre visando a<br/> satisfação total de nossos clientes.<br/>
       Se você busca inovação e soluções digitais que<br/> fazem a diferença, a MTHS Developer está<br/> pronta para ser o parceiro ideal em sua jornada<br/> digital.</p>
       <img className='img02' src={IMG02 }/>
+      <h2 className='portifolio'>Portifólio</h2>
       <Swiper
       spaceBetween={50}
       slidesPerView={3}
